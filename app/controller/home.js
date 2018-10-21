4a8aa652aac5688f9359f9e8e83c86c370ce3a74
Elven Lin <elvenlin1998@gmail.com>
2018-10-22 01:32:00 +0800

    test For

--- app/controller/home.js
+++ app/controller/home.js
@@ -6,6 +6,12 @@ class HomeController extends Controller {
   async index() {
     this.ctx.body = 'hi, egg';
   }
+  async getLocation() {
+    this.ctx.body = {
+      currentLocation: 'Taiwan',
+      time: '2018/08/12'
+    }
+  }
 }
 
 module.exports = HomeController;
