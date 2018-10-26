2f4abe34635ead16e9c75fddf34b425961fa5925
Elven Lin <elvenlin1998@gmail.com>
2018-10-27 07:31:44 +0800

    setup cors

--- config/config.default.js
+++ config/config.default.js
@@ -3,23 +3,20 @@
 module.exports = appInfo => {
   const config = exports = {};
 
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1540137642708_4586';
+
+  // add your config here
+  config.middleware = [];
+
   /**
    * CORS 插件設定
    * - egg-cors
    */
   config.cors = {
     origin: 'http://localhost:8080',
-    allowHeaders: '*',
-    allowMethods: 'GET,HEAD,PUT,POST,DELETE'
-  }
-
-  /**
-   * CSRF 攻擊防護設定
-   */
-  config.security = {
-    csrf: {
-      ignore: ctx => true
-    }
+    allowHeaders: 'content-type',
+    allowMethods: 'GET,POST'
   }
 
   return config;
