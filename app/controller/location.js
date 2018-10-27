020dd0addac92ba9557684d81e836790cae9eb91
Elven Lin <elvenlin1998@gmail.com>
2018-10-28 05:37:47 +0800

    fake api

--- app/controller/location.js
+++ app/controller/location.js
@@ -2,53 +2,37 @@
 
 const Controller = require('egg').Controller;
 
-class UserController extends Controller {
-  async login() {
+class LocationController extends Controller {
+  async addFavoriteLocation() {
     const { ctx, app, logger } = this;
     const { global_dataStructure } = app;
     const { request, response } = ctx;
     const { body } = request;
     const { email, password, name } = body;
 
-    if (typeof email === "undefined" || typeof password === "undefined") ctx.throw(400, 'Bad request: inssuficient parameters');
-
-    global_dataStructure.currentUser = {
-      email: email,
-      password: password,
-      name: name,
-      islogin: false
-    }
+    global_dataStructure.favoriteLocationList.push(body);
 
     response.body = {
       success: true
     }
   }
-  async getCurrentUser() {
+  async getCurrentListOfLocation() {
     const { ctx, app, logger} = this;
     const { global_dataStructure } = app;
     const { request, response } = ctx;
     const { body } = request;
 
-    response.body = global_dataStructure.currentUser;
+    response.body = global_dataStructure.favoriteLocationList;
   }
-  async logout() {
+  async removeFavoriteLocation() {
     const { ctx, app, logger } = this;
     const { global_dataStructure } = app;
     const { request, response } = ctx;
     const { body } = request;
     const { email, password } = body;
 
-    global_dataStructure.currentUser = {
-      email: "",
-      password: "",
-      name: "",
-      islogin: false
-    }
-
-    response.body = {
-      success: true
-    }
+    
   }
 }
 
-module.exports = UserController;
+module.exports = LocationController;
--- app/router.js
+++ app/router.js
@@ -11,6 +11,12 @@ module.exports = app => {
   // Add Axios to global.
   app.axios = axios;
 
+  app.global_dataStructure = {
+    currentUser: {},
+    favoriteLocationList: [],
+    samplePoints: []
+  }
+
   // Search
   router.post('/search/findNearBy', controller.search.findNearBy);
   router.post('/search/findPlaces', controller.search.findPlaces);
@@ -21,5 +27,15 @@ module.exports = app => {
 
   // User
   router.post('/user/login', controller.user.login);
-  
+  router.get('/user/getCurrentUser', controller.user.getCurrentUser);
+  router.get('/user/logout', controller.user.logout);
+
+  // Location
+  router.post('/location/addFavoriteLocation', controller.location.addFavoriteLocation);
+  router.get('/location/getCurrentListOfLocation', controller.location.getCurrentListOfLocation);
+
+  // Sample Point
+  router.post('/sampleLocation/addSample', controller.sampleLocation.addSample)
+  router.get('/sampleLocation/getAllSample', controller.sampleLocation.getAllSample)
+
 };
