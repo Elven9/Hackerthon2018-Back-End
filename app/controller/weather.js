a2a0ed98a594641f887c8908bf02aa65ac9e9bfd
jackys940194 <jackys940194@gmail.com>
2018-10-27 16:22:52 +0800

    Weather was finished by jacky!

--- app/controller/weather.js
+++ app/controller/weather.js
@@ -8,12 +8,60 @@ class WeatherController extends Controller {
   async index() {
     const { ctx, app, logger } = this;
     const { request, response } = ctx;
-
-    console.log(request.body);
-
-    const result = await requestWeather('v1/rest/datastore/F-C0032-001', "GET", request.body);
+    const { body } = request;
+    //Body 
+    
+    body.startTime = body.startTime.replace(/:/g,"%3A");
+    body.locationName = encodeURI(body.locationName);
+    
+    const result = await requestWeather('v1/rest/datastore/F-C0032-001', "GET", body);
+    const gave = {
+      icon: 0, //icon from 0 to 4   -----> 0 for 晴天, 1 for 多雲, 2 for 陰天, 3 for 小雨, 4 for 大雨
+      minT: 0,
+      maxT: 0
+    };
+    switch(result.data.records.location[0].weatherElement[0].time[0].parameter.parameterValue){
+      case 1 :
+      case 8 :
+      case 43:
+      case 46:
+        gave.icon = 0;
+        break;
+      case 2 :
+      case 7 :
+      case 45:
+        gave.icon = 1;
+        break;
+      case 3 :
+      case 5 :
+      case 6 :
+      case 44:
+        gave.icon = 2;
+        break;
+      case 4 :
+      case 24:
+      case 26:
+      case 49:
+      case 57:
+      case 60:
+        gave.icon = 3;
+        break;
+      case 12:
+      case 13:
+      case 17:
+      case 18:
+      case 31:
+      case 34:
+      case 36:
+      case 58:
+      case 59:
+        gave.icon = 4;
+        break;
+    }
+    gave.minT = result.data.records.location[0].weatherElement[2].time[0].parameter.parameterName;
+    gave.maxT = result.data.records.location[0].weatherElement[4].time[0].parameter.parameterName;
     // Code
-    response.body = result.data;
+    response.body = { result : gave, raw : result.data};
   }
 }
 
